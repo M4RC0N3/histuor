85dcd724abbcfc4f8c32c4a7099e4d486d17eb41
M4RC0N3 <marcone@quickup.io>
2023-10-14 20:28:24 -0300

    add trip component and implement trip list

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -5,9 +5,10 @@ import { TabNavigation } from './tab-navigation/tab-navigation';
 import { IonicModule } from '@ionic/angular';
 import { Categorie } from './categorie/categorie';
 import { Place } from './place/place';
+import { Trip } from './trip/trip';
 @NgModule({
   imports: [CommonModule, IonicModule],
-  declarations: [TabNavigation, Categorie, Place],
-  exports: [TabNavigation, Categorie, Place] // Exporte os componentes que deseja compartilhar
+  declarations: [TabNavigation, Categorie, Place, Trip],
+  exports: [TabNavigation, Categorie, Place, Trip] // Exporte os componentes que deseja compartilhar
 })
 export class ComponentsModule {}
