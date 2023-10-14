beae83949030db50efeaaf2395ef674e1811f098
M4RC0N3 <marcone@quickup.io>
2023-10-14 19:09:27 -0300

    add place component

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { TabNavigation } from './tab-navigation/tab-navigation';
 import { IonicModule } from '@ionic/angular';
 import { Categorie } from './categorie/categorie';
+import { Place } from './place/place';
 @NgModule({
   imports: [CommonModule, IonicModule],
-  declarations: [TabNavigation, Categorie],
-  exports: [TabNavigation, Categorie] // Exporte os componentes que deseja compartilhar
+  declarations: [TabNavigation, Categorie, Place],
+  exports: [TabNavigation, Categorie, Place] // Exporte os componentes que deseja compartilhar
 })
 export class ComponentsModule {}
